553195d6e89d7f5828e0e0eb20ad3740c6cca1b2
albrecht eric <e.albrecht@laposte.net>
2020-06-05 19:54:55 +0200

    fist commit

---
+++ app/src/main/java/fr/kefera/exemple_appbar/MainActivity.kt
@@ -0,0 +1,32 @@
+package fr.kefera.exemple_appbar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if(item != null){
+            when (item.itemId){
+                R.id.settings-> displaySettings()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    fun displaySettings(){
+        println("Affichage des réglages")
+    }
+}
